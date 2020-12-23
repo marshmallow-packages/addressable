699b10f42ab36901898b091a6ce8461f2127df19
Stef van Esch <stef@marshmallow.dev>
2020-12-23 18:12:42 +0100

    Default logic

--- src/Models/Address.php
+++ src/Models/Address.php
@@ -3,7 +3,9 @@
 namespace Marshmallow\Addressable\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Marshmallow\Addressable\Models\AddressType;
 use Marshmallow\Datasets\Country\Models\Country;
 
 class Address extends Model
@@ -11,6 +13,70 @@ class Address extends Model
     use SoftDeletes;
 
     protected $guarded = [];
+
+    protected static function booted()
+    {
+        self::created(function ($address) {
+            $has_default = self::sameOwner($address)
+                                ->default()
+                                ->first();
+            if (!$has_default) {
+                $address->makeDefault();
+            }
+        });
+
+        self::deleted(function ($address) {
+            $has_default = self::sameOwner($address)
+                                ->notThisOne($address)
+                                ->default()
+                                ->first();
+            if (! $has_default) {
+                $first_result = self::sameOwner($address)->notThisOne($address)->first();
+                if ($first_result) {
+                    $first_result->makeDefault();
+                }
+            }
+        });
+    }
+
+    public function makeDefault()
+    {
+        $defaults = self::sameOwner($this)
+                        ->default()
+                        ->get();
+
+        foreach ($defaults as $default) {
+            $default->update([
+                'default' => false,
+            ]);
+        }
+
+        $this->update([
+            'default' => true,
+        ]);
+    }
+
+    public function scopeNotThisOne(Builder $builder, Model $owner)
+    {
+        $builder->where('id', '!=', $owner->id);
+    }
+
+    public function scopeSameOwner(Builder $builder, Model $owner)
+    {
+        $builder->where('addressable_type', $owner->addressable_type)
+                ->where('addressable_id', $owner->addressable_id)
+                ->where('address_type_id', $owner->address_type_id);
+    }
+
+    public function scopeDefault(Builder $builder)
+    {
+        $builder->where('default', 1);
+    }
+
+    public function scopeWhereType(Builder $builder, AddressType $type)
+    {
+        $builder->where('address_type_id', $type->id);
+    }
 
     public function country()
     {
