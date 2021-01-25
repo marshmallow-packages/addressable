c05c377df88c6b81dbf29c0523115a4c75bfcd38
Stef van Esch <stef@marshmallow.dev>
2021-01-25 17:10:05 +0100

    Get the default address by type

--- src/Traits/Addressable.php
+++ src/Traits/Addressable.php
@@ -3,11 +3,18 @@
 namespace Marshmallow\Addressable\Traits;
 
 use Marshmallow\Addressable\Models\Address;
+use Marshmallow\Addressable\Models\AddressType;
 
 trait Addressable
 {
     public function addresses()
     {
         return $this->morphMany(Address::class, 'addressable');
+    }
+
+    public function getDefaultAddress(string $type)
+    {
+        $type = AddressType::where('type', $type)->firstOrFail();
+        return $this->addresses()->whereType($type)->default()->first();
     }
 }
